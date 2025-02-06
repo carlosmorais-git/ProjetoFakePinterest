fb618cefa2638d987e7477ad491b9c829113adad
carlos silvano morais <127549934+carlosmorais-git@users.noreply.github.com>
2025-02-06 16:08:06 -0300

    Update __init__.py

--- projeto/__init__.py
+++ projeto/__init__.py
@@ -31,16 +31,16 @@ logger = logging.getLogger(__name__) # Cria um logger específico para o módulo
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ce4a8b123dc22d762d3086ffe953071a'
 
-# Define a pasta de uploads, verificando primeiro se há uma variável de ambiente
-# Se a variável UPLOAD_FOLDER estiver definida, usa esse valor
-# Caso contrário, usa "static/fotos_posts" como padrão
+# # Define a pasta de uploads, verificando primeiro se há uma variável de ambiente
+# # Se a variável UPLOAD_FOLDER estiver definida, usa esse valor
+# # Caso contrário, usa "static/fotos_posts" como padrão
+
 UPLOAD_FOLDER = os.getenv("UPLOAD_FOLDER", os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/fotos_posts"))
 
-# Garante que a pasta de uploads existe, criando-a caso não exista
+# Garante que a pasta de uploads existe no ambiente de produção e local
 if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-# Configura a pasta de uploads no Flask
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 
